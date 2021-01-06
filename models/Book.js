99b25fdf6832f4a0d787230d0f28355c4511f6a9
Joao C Barbosa <joaao@outlook.com>
2021-01-06 17:47:21 -0300

    refact: refact code

--- models/Book.js
+++ models/Book.js
@@ -1,22 +1,22 @@
-var mongoose = require('mongoose');
+const mongoose = require('mongoose');
 // var mongoosePaginate = require('mongoose-paginate');
 
-var schema = mongoose.Schema({
-    name: { 
-        type: String, 
+const schema = mongoose.Schema({
+    name: {
+        type: String,
         required: true
-    }, 
+    },
     price: {
         required: true,
         type: String,
-    }, 
+    },
     description: {
         required: true,
-        type: String        
+        type: String
     },
     cover: {
         required: true,
-        type: String        
+        type: String
     }
 
 });
